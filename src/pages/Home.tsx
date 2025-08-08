f8f4a6f5769ccdfb25389cdbe890d8c33369c66f
elliebean <git@beans.dog>
2025-08-08 15:27:30 +0100

    Add update task mutation and update isCompleted on checkbox toggle

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,67 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { NavLink } from "react-router";
 import Card from "../components/Card";
-import { getTasks } from "../services/mock-api";
+import { getTasks, updateTask, type Task, type UpdateTaskData } from "../services/mock-api";
 
 function Home() {
+  const queryClient = useQueryClient();
   const query = useQuery({ queryKey: ["tasks"], queryFn: getTasks });
 
-  console.log(query.data);
+  const updateTaskMutation = useMutation({
+    mutationFn: ({ id, updates }: { id: string; updates: UpdateTaskData }) => updateTask(id, updates),
+
+    onMutate: async ({ id, updates }) => {
+      await queryClient.cancelQueries({ queryKey: ["tasks"] });
+      const previousTasks = queryClient.getQueryData(["tasks"]);
+
+      // Optimistically update cache with new data
+      queryClient.setQueryData<Task[]>(["tasks"], (oldQueryData) => {
+        return oldQueryData?.map((task) => {
+          if (task.id === id) {
+            return { ...task, ...updates };
+          } else {
+            return task;
+          }
+        });
+      });
+
+      // Return previous state to rollback after errors
+      return { previousTasks };
+    },
+
+    onError: (_error, _variables, context) => {
+      // Rollback optimistic update on error
+      queryClient.setQueryData(["tasks"], context?.previousTasks);
+    },
+
+    onSuccess: (data, variables) => {
+      // Update cache with response data to avoid refreshing
+      queryClient.setQueryData<Task[]>(["tasks"], (oldQueryData) => {
+        return oldQueryData?.map((task) => {
+          if (task.id === variables.id) {
+            return data;
+          } else {
+            return task;
+          }
+        });
+      });
+    },
+  });
+
+  const handleCompletionCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>, taskId: string) => {
+    updateTaskMutation.mutate({ id: taskId, updates: { isCompleted: event.target.checked } });
+  };
 
   return (
     <ul className="flex flex-col gap-2 w-full max-w-2xl">
       {query.data?.map((task) => (
         <Card key={task.id}>
           <div className="flex items-center">
-            <input type="checkbox" checked={task.isCompleted} />
+            <input
+              type="checkbox"
+              checked={task.isCompleted}
+              onChange={(event) => handleCompletionCheckboxChange(event, task.id)}
+            />
             <div className="flex flex-col grow">
               <span>{task.title}</span>
               <span>{task.description}</span>
